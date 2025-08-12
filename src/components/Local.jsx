34d951cffd8263b726f29c9dcb38bc1cf439b4c4
Deco <andremarcomagalhaes@gmail.com>
2025-08-12 19:45:39 -0300

    Css mobile changes

--- src/components/Local.jsx
+++ src/components/Local.jsx
@@ -112,14 +112,18 @@ function PlayLocal() {
 
         <div className="board-container">
           <img src={boardImage} alt="Tic Tac Toe Board" className="board-image" />
+          
           <div className="board-overlay">
             {Array(9).fill(null).map((_, index) => renderSquare(index))}
           </div>
+
+
           <button className="reset-button" onClick={resetGame} title="New Game">
             <svg viewBox="0 0 24 24" fill="currentColor">
               <path d="M12 5V1L7 6l5 5V7c3.31 0 6 2.69 6 6s-2.69 6-6 6-6-2.69-6-6H4c0 4.42 3.58 8 8 8s8-3.58 8-8-3.58-8-8-8z"/>
             </svg>
           </button>
+
         </div>
       </div>
     </div>  
