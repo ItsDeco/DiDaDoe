25b5aa6961bcf162ae1a8d37f2db1fe96ff1b32c
Deco <andremarcomagalhaes@gmail.com>
2025-08-17 16:35:18 -0300

    Style changes

--- src/components/Online.jsx
+++ src/components/Online.jsx
@@ -1,9 +1,10 @@
 import { Link } from 'react-router-dom'
+import './Game.css'
 
 function PlayOnline() {
   return (
     <div>
-      <h1 style={{ textAlign: 'center', padding: '0vh'}}>Play Online</h1>
+      <h1 style={{animation: 'fadeIn 0s'}}>Play Online</h1>
       <p style={{ fontStyle: 'italic', color: '#999', justifyContent: 'center', display: 'flex', padding: '2vh 0'}}>
         Coming soon!</p>
       <Link to="/">
